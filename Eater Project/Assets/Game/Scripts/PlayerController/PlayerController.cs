ecf328358fed83bcd0dd386eb63f4fc57ad310f3
İbrahim karabay <ibrahim.karabay.95@gmail.com>
2022-08-05 14:14:02 +0300

    controlinput

---
+++ Eater Project/Assets/Game/Scripts/PlayerController/PlayerController.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private PlayerState myState;
+    private Joystick myJoystick;
+    [SerializeField] private float speed;
+    
+    public PlayerState State
+    {
+        get
+        {
+            return myState;
+        }
+        set
+        {
+            if (State == value)
+            {
+                return;
+            }
+            myState = value;
+            OnStateChanges();
+        }
+    }
+
+    private void OnStateChanges()
+    {
+        switch (State)
+        {
+            case PlayerState.IDLE:
+                
+                break;
+            case PlayerState.RUN:
+                
+                break;
+            default:
+                break;
+        }
+    }
+    void Start()
+    {
+        myJoystick = GameManager.Instance.Joystick;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMoveControl();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<IPlayerCollectable>()!=null)
+        {
+            other.GetComponent<IPlayerCollectable>().DoPlayerCollect();
+        }
+    }
+
+    private void PlayerMoveControl()
+    {
+        if (myJoystick.Direction.x != 0 || myJoystick.Direction.y != 0)
+        {
+            State = PlayerState.RUN;
+        }
+        else
+        {
+            State = PlayerState.IDLE;
+        }
+        Vector3 myDirection = new Vector3(myJoystick.Direction.x, 0, myJoystick.Direction.y).normalized;
+        transform.Translate(myDirection * Time.deltaTime * speed, Space.World);
+        Vector3 lookAtPoz = transform.position + myDirection;
+        transform.LookAt(lookAtPoz);
+    }
+}
